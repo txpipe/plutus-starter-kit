3fb2c5d1772e104a0d9734007deac9c059e3d957
Mawuko Batse <mawukobatse@hotmail.com>
2022-10-19 13:05:58 -0400

    Update download-params.sh
    The ReadMe/Guide for the starter project uses "params.json" when building the TX, but the script to download that file saves it as "pparams.json".
    
    Figured to make that edit at the .sh file instead of the readme doc.

--- scripts/download-params.sh
+++ scripts/download-params.sh
@@ -1,3 +1,3 @@
 here=$(dirname $0)
 
-cardano-cli query protocol-parameters --testnet-magic $CARDANO_NODE_MAGIC > ${here}/../assets/pparams.json
+cardano-cli query protocol-parameters --testnet-magic $CARDANO_NODE_MAGIC > ${here}/../assets/params.json
